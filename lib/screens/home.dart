f67db5ffaa5752278e6996d7e579a79521c50fc6
Adokiye <gorgeceo@gmail.com>
2020-05-31 20:48:39 +0100

    Fixes

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -53,39 +53,6 @@ class _MyHomePageState extends State<MyHomePage> {
             return CircularProgressIndicator();
           },
         ),
-      
-      drawer: Drawer(
-        child: ListView(
-          // Important: Remove any padding from the ListView.
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            DrawerHeader(
-              child: Text('Xenforo Forum'),
-            //   decoration: BoxDecoration(
-            //  //   color: Colors.blue,
-            //   ),
-            ),
-            ListTile(
-              title: Text('Item 1'),
-              onTap: () {
-                // Update the state of the app
-                // ...
-                // Then close the drawer
-                Navigator.pop(context);
-              },
-            ),
-            // ListTile(
-            //   title: Text('Item 2'),
-            //   onTap: () {
-            //     // Update the state of the app
-            //     // ...
-            //     // Then close the drawer
-            //     Navigator.pop(context);
-            //   },
-            // ),
-          ],
-        ),
-      ),
     );
   }
 }
